4ceffc380c24ad04ec16e95b1aefc6717c3b5f17
thiago-santanna <uzmaki_thiago@outlook.com>
2023-03-09 09:31:15 -0300

    primeiro commit

---
+++ src/main/java/com/tsswebapps/smallfinances/SmallFinancesApplication.java
@@ -0,0 +1,13 @@
+package com.tsswebapps.smallfinances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallFinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallFinancesApplication.class, args);
+	}
+
+}
